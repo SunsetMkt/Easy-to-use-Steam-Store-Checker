7a42baed57dbd98ce37c62c41bf0b8cb8349c1ab
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-02-08 00:10:47 +0800

    Fix utf8 error with hack

--- src/res/curl.bat
+++ src/res/curl.bat
@@ -8,25 +8,32 @@ if %3 == 80 (set protocol=http://)
 if %3 == 443 (set protocol=https://)
 set url=%protocol%%domain%
 set basetitle=curl - %domain%解析为%ip%
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 
 title %basetitle%
 
 echo curl %ip%网站访问测试
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 echo 在本测试中，我们会以访问%domain%的名义尝试访问服务器%ip%。URL为%url%，其中%domain%被解析为%ip%。
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 echo 请按下任意键测试一次
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 pause > nul
 
 :curl
 title %basetitle% - 请求中
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 
 .\curl.exe -I -k -v --resolve %domain%:%port%:%ip% %url%
 set lastreturn=%errorlevel%
 
 title %basetitle% - 请求结束
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 
 if %lastreturn% == 0 (echo 请求成功) else (echo 请求失败)
 
 echo 请按下任意键再测试一次
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 pause > nul
 
 goto curl
--- src/res/tcping.bat
+++ src/res/tcping.bat
@@ -8,8 +8,11 @@ set basetitle=tcping - %ip%:%port%
 title %basetitle%
 
 echo tcping %ip%服务器%port%端口TCP协议可达性测试
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 echo 在本测试中，我们会不断测试%ip%:%port%是否可达。若不可达，则会提示“No Response”。
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 echo 请仔细观察下方输出信息。
+echo chineseUTF8fixHACKdoNOTremoveTHISline > nul
 
 .\tcping.exe -t %ip% %port%
 
